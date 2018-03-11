aa7ef8b84f7b244eaec01c9f4f37f68a7c43ed86
Hoang Danh <hoangdanhlx1998@gmail.com>
2018-03-12 02:41:12 +0700

    Update
    big Update

---
+++ src/js/init-info-kpop.js
@@ -0,0 +1,181 @@
+var info = [
+    {
+        id: 'V',
+        srcImg: 'src/img/BackGround/V.jpg',
+        info: [
+            {
+                title: 'Thông tin cá nhân: V',
+                content: [
+                    "Ngày sinh: 30/12/1995 (20 tuổi)",
+                    "Cung hoàng đạo: Ma Kết.",
+                    "Tên Tiếng Trung: 金泰亨 (Kim Tae Hyung)",
+                    "Nhóm máu: AB",
+                    "Chiều cao: 1m79",
+                    "Cân nặng: 58kg",
+                    "Công ty chủ quản: Big Hit Entertainment, Pony Canyon.",
+                    "Facebook: https://www.facebook.com/Taehyung.02/",
+                    "Instagram: https://www.instagram.com/kingtaehyung/",
+                    "Trang web chính thức: http://bts.ibighit.com/"
+                ]
+            },
+            {
+                title: 'Tiểu sử bản thân và sự nghiệp:',
+                content: [
+                    "V sinh ra tại Daegu, Hàn Quốc, gia đình anh gồm bố, mẹ, em gái Kim Eunjin và em trai Kim Jonggyu. Anh học phổ thông tại trường “Daegu first hight school” và anh không học đại học.",
+                    "V tham gia thử giọng và là thực tập sinh của Big Hit từ năm 2011 nhưng công ty giữ bí mật này cho đến năm 2013.",
+                    "Ngày 13 tháng 06 năm 2013, anh xuất hiện lần đầu trên chương trình M!Countdown với ca khúc “No More Dream” cùng single đầu tiên trong sự nghiệp “2 COOL 4 SKOOL”.",
+                    "Nghệ danh của anh nghĩa là “Victory”, là đại diện cho sự thành công của nhóm. Nghệ danh này được chọn bởi CEO của Big Hit, Bang Si Hyuk.",
+                    "Vai trò của Kim trong nhóm là hát dẫn (Lead singer). Ngoài ra anh cũng tham gia sản xuất và viết một vài ca khúc trong nhóm, tiêu biểu là “Hold me tight”.",
+                    "Năm 2016, “V” lần đầu tiên chuyển mình sang sân khấu điện ảnh với vai nam phụ “Han Sung” trong bộ phim truyền hình “Hwarang: The Beginning” của đài KBS2.",
+                    "Anh tham gia khá nhiều chương trình TV show trải dài từ năm 2013 đến nay, tiêu biểu như “After School Club” (Ep 24, 46, 95, 158, 191); “Star King” (Ep 413) , “Weekly Idol” (Ep 144, 203, 229).",
+                    "Anh cũng làm MC đặc biệt cho các chương trình: “Show!Music Core” (2014); “Best of the Best” (4 Ep với Suejeong của Lovelyz), “Music Bank” (Ep 814 với Park Bo Gum và Himchan nhóm B.A.P) (2015); “Inkigayo” (Ep 868 với J-Hope)"
+
+                ]
+            },
+            {
+                title: 'Giải thưởng:',
+                content:[
+                    "MelOn Music Award : New artist of the year (2013)",
+                    "Golden Disk Award: Newcomer Award (2014)",
+                    "Gaon Chart K-Pop Award: ￼New Artist of the Year (Male Group) (2014)",
+                    "MelOn Music Award: Best Male Dance Award (2015)",
+                    "Gaon Chart K-Pop Award: ￼World Rookie Award (2015)",
+                    "Golden Disk Award: Disk Bongsang (2016)",
+                    "Seoul Music Award: Bonsang Award (2016)",
+                    "Gaon Chart K-Pop Award: K-Pop World Hallyu Star Award (2016)"
+                ]
+            },
+            {
+                title: 'Fun Facts:',
+                content:[
+                    "Cha anh là hình mẫu mà anh luôn theo đuổi.",
+                    "Size giày: 7.5",
+                    "Theo đạo Thiên Chúa Giáo.",
+                    "Có thể nói tiếng Nhật và tiếng Anh.",
+                    "Là thành viên có thời gian thực tập lâu nhất nhóm.",
+                    "Phong cách trang điểm của anh là trang điểm tự nhiên.",
+                    "Anh thích đùa giỡn với các A.R.M.Y trong các buổi họp fan"
+                ]
+            }
+        ]
+    },
+    // -----------------------------------------------------------------------------------------------
+    {
+        id: 'Suga',
+        srcImg: 'src/img/background/suga.jpg',
+        info: [
+            {
+                title: 'Thông tin cá nhân:',
+                content: [
+                    "Ngày sinh: 30/12/1995 (20 tuổi)",
+                    "Cung hoàng đạo: Ma Kết.",
+                    "Tên Tiếng Trung: 金泰亨 (Kim Tae Hyung)",
+                    "Nhóm máu: AB",
+                    "Chiều cao: 1m79",
+                    "Cân nặng: 58kg",
+                    "Công ty chủ quản: Big Hit Entertainment, Pony Canyon.",
+                    "Facebook: https://www.facebook.com/Taehyung.02/",
+                    "Instagram: https://www.instagram.com/kingtaehyung/",
+                    "Trang web chính thức: http://bts.ibighit.com/"
+                ]
+            },
+            {
+                title: 'Tiểu sử bản thân và sự nghiệp:',
+                content: [
+                    "Quê hương của Suga là Daegu, Hàn Quốc (cùng quê với V). Anh học tại đại học Global Cyber University (Seoul), chuyên ngành Nghệ thuật biểu diễn sân khấu (Broadcasting Performing Art). Suga nói rất thành thạo tiếng Anh.",
+                    "Gia đình anh gồm có bố, mẹ, anh trai và anh.",
+                    "Trước khi BTS debut. Suga cùng Jin và Rap Monster ra mắt ca khúc “School of Tears”, cover từ “Drank” của Kendrick Lanmar, bài hát nói về cuộc sống trong xã hội hiện đại cùng với bệnh thành tích ở môi trường giáo dục.",
+                    "Ngoài vai trò chính là Main Rapper, Suga cũng tham gia viết nhạc. Bài “Boy in Luv” (ra mắt vào ngày 12/02/2014) trong mini album thứ 2 “Skool Luv Affair” được anh cùng với leader Rap Monster và các nhạc sỹ sản xuất. Ca khúc này được đề cử No.1 trong chương trình “Inkigayo” (SBS) và là ca khúc đầu tiên của nhóm được đề cử trong vòng chưa đầy một năm ra mắt. Mini album này có sức ảnh hưởng rất lớn đến làng giải trí xứ kim chi tại thời điểm ra mắt.",
+                    "Ngày 18/11/2015, đoạn rap teaser trong bài “Never Mind” do Sugar thể hiện ra mắt. Đoạn rap khởi đầu cho sự comeback của BTS và đã lọt vào top trend của thế giới chỉ sau vài phút với hashtag #Nevermind. Tại Hàn Quốc #Nevermind là từ khóa được tìm kiếm nhiều nhất ở Naver (một trang web dạng như Google của Hàn Quốc) vào thời điểm này.",
+                    "Ngày 16/08/2016, với nghệ danh “Agust D”, Suga ra mắt mixtape solo đầu tiên trong sự nghiệp với ca khúc chủ đề “Agust D”. Hai MV được chọn để ra mắt các fan trong album này là “Agust D” và “Give it to me”.",
+                    "Cũng như các thành viên khác, Suga tham gia rất nhiều TV show, radio show nhằm mục đích phổ biến hình ảnh của nhóm đến khán giả."
+                ]
+            },
+            {
+                title: 'Giải thưởng',
+                content:[
+                    "MelOn Music Award : New artist of the year (2013)",
+                    "Golden Disk Award: Newcomer Award (2014)",
+                    "Gaon Chart K-Pop Award: ￼New Artist of the Year (Male Group) (2014)",
+                    "MelOn Music Award: Best Male Dance Award (2015) ",
+                    "Gaon Chart K-Pop Award: ￼World Rookie Award (2015)",
+                    "Golden Disk Award: Disk Bongsang (2016)",
+                    "Seoul Music Award: Bonsang Award (2016)",
+                    "Gaon Chart K-Pop Award: K-Pop World Hallyu Star Award (2016)"
+                ]
+            }
+
+        ]
+    },
+    //--------------------------------------------------------------------------
+    {
+        id: 'JungKook',
+        srcImg: 'src/img/background/JungKook.jpg',
+        info: [
+            {
+                title: 'Thông tin cá nhân:',
+                content: [
+                    "Tên thật của  Jin trong nhóm BTS: Kim Seok Jin",
+                    "Nghệ danh : Pink Princess, Jin Princess, Anh Vai Đẹp, Cỗ Máy Nhảy, Eat Jin.",
+                    "Ngày sinh : 04/12/1992 —-> Nhân Mã",
+                    "Vị trí : Vocalist, Visual (gương mặt của nhóm)",
+                    "Nhóm máu : O",
+                    "Chiều cao: 1m79cm",
+                    "Cân nặng: 60kg",
+                    "Công ty chủ quản: Big Hit Entertainment, Pony Canyon."
+                ]
+            },
+            {
+                title:'Fun Facts',
+                content:[
+                    "Nấu ăn ngon nhất nhóm.",
+                    " Giỏi tiếng Quan Thoại (tiếng phổ thông Trung Quốc)",
+                    " Thích các nhân vật công chúa và chó con của Disney.",
+                    "Anh ấy là người duy nhất tham gia thử giọng khi vẫn đang đi học.",
+                    "Theo Jin, khi anh ấy còn là học sinh, anh ấy khá trầm tính nhưng cũng không (đến mức) phải ngồi ăn một mình",
+                    " Jin nói rằng sẽ hẹn hò với Jimin nếu Jin là con gái bởi vì anh ấy không có tính hướng ngoại cao, vậy nên sẽ tốt hơn nếu hẹn hò với 1 người như Jimin.",
+                    " Bây giờ Jin đang học cách làm diễn viên bằng cách học mấy kiểu quyến rũ các ahjumma (thím/ gái chưa chồng) và tất cả các eomma (bà mẹ/ bà nội trợ)",
+                    "Thánh vụng về của BTS, vụng về trong mọi chuyện =))",
+                    "Ham ăn nhất nhóm"
+                ]
+            }
+        ]
+        },
+    {
+        id:'Jimin',
+        srcImg:'src/img/background/Jmin.jpg',
+        info:[
+            {
+                title:'Thông tin cá nhân:',
+                content:[
+                    "Tên thật : Park Jimin",
+                    "Biệt danh : ChimChim, Chim Lùn, Con Heo, Mều"
+                ]
+            }
+        ]
+    }
+]
+
+// console.log(info[0].info[2].title);
+
+for (var i = 0; i < info.length; ++i) {
+    var parentDIV = document.getElementById(info[i].id);
+    var myImg = document.createElement("img");
+    myImg.src = info[i].srcImg;
+    parentDIV.appendChild(myImg);
+    for (var j = 0; j < info[i].info.length; ++j) {
+        var myHeader = document.createElement("h1");
+        myHeader.innerHTML = info[i].info[j].title;
+        parentDIV.appendChild(myHeader);
+
+        var myUL = document.createElement("ul");
+        parentDIV.appendChild(myUL);
+        for (var k = 0; k < info[i].info[j].content.length; ++k) {
+            var myLI = document.createElement("li");
+            myLI.innerHTML = info[i].info[j].content[k];
+            myUL.appendChild(myLI);
+        }
+
+
+    }
+}
