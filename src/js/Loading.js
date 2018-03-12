8c98c0369e80235eda6f46617f73b29df8e41ce1
Hoang Danh <hoangdanhlx1998@gmail.com>
2018-03-13 02:40:49 +0700

    reupdate background

--- src/js/Loading.js
+++ src/js/Loading.js
@@ -1,24 +1,370 @@
+var info = [
+    {
+        id: 'V',
+        srcImg: 'src/img/BackGround/V.jpg',
+        info: [
+            {
+                title: 'Thông tin cá nhân: V',
+                content: [
+                    "Ngày sinh: 30/12/1995 (20 tuổi)",
+                    "Cung hoàng đạo: Ma Kết.",
+                    "Tên Tiếng Trung: 金泰亨 (Kim Tae Hyung)",
+                    "Nhóm máu: AB",
+                    "Chiều cao: 1m79",
+                    "Cân nặng: 58kg",
+                    "Công ty chủ quản: Big Hit Entertainment, Pony Canyon.",
+                    "Facebook: https://www.facebook.com/Taehyung.02/",
+                    "Instagram: https://www.instagram.com/kingtaehyung/",
+                    "Trang web chính thức: http://bts.ibighit.com/"
+                ]
+            },
+
+            {
+                title: 'Giải thưởng:',
+                content:[
+                    "MelOn Music Award : New artist of the year (2013)",
+                    "Golden Disk Award: Newcomer Award (2014)",
+                    "Gaon Chart K-Pop Award: ￼New Artist of the Year (Male Group)(2014)",
+                    "MelOn Music Award: Best Male Dance Award (2015)",
+                    "Gaon Chart K-Pop Award: ￼World Rookie Award (2015)",
+                    "Golden Disk Award: Disk Bongsang (2016)",
+                    "Seoul Music Award: Bonsang Award (2016)",
+                    "Gaon Chart K-Pop Award: K-Pop World Hallyu Star Award (2016)"
+                ]
+            },
+
+            {
+                title: 'Fun Facts:',
+                content:[
+                    "Cha anh là hình mẫu mà anh luôn theo đuổi.",
+                    "Size giày: 7.5",
+                    "Theo đạo Thiên Chúa Giáo.",
+                    "Có thể nói tiếng Nhật và tiếng Anh.",
+                    "Là thành viên có thời gian thực tập lâu nhất nhóm.",
+                    "Phong cách trang điểm của anh là trang điểm tự nhiên.",
+                    "Anh thích đùa giỡn với các A.R.M.Y trong các buổi họp fan"
+                ]
+            }
+        ]
+    },
+    // -----------------------------------------------------------------------------------------------
+    {
+        id: 'Suga',
+        srcImg: 'src/img/background/suga.jpg',
+        info: [
+            {
+                title: 'Thông tin cá nhân: Suga',
+                content: [
+                    "Ngày sinh: 30/12/1995 (20 tuổi)",
+                    "Cung hoàng đạo: Ma Kết.",
+                    "Tên Tiếng Trung: 金泰亨 (Kim Tae Hyung)",
+                    "Nhóm máu: AB",
+                    "Chiều cao: 1m79",
+                    "Cân nặng: 58kg",
+                    "Công ty chủ quản: Big Hit Entertainment, Pony Canyon.",
+                    "Facebook: https://www.facebook.com/Taehyung.02/",
+                    "Instagram: https://www.instagram.com/kingtaehyung/",
+                    "Trang web chính thức: http://bts.ibighit.com/"
+                ]
+            },
+
+            {
+                title: 'Giải thưởng',
+                content:[
+                    "MelOn Music Award : New artist of the year (2013)",
+                    "Golden Disk Award: Newcomer Award (2014)",
+                    "Gaon Chart K-Pop Award: ￼New Artist of the Year (Male Group) (2014)",
+                    "MelOn Music Award: Best Male Dance Award (2015) ",
+                    "Gaon Chart K-Pop Award: ￼World Rookie Award (2015)",
+                    "Golden Disk Award: Disk Bongsang (2016)",
+                    "Seoul Music Award: Bonsang Award (2016)",
+                    "Gaon Chart K-Pop Award: K-Pop World Hallyu Star Award (2016)"
+                ]
+            },
+
+			{
+			title: 'Fun Facts:',
+                content:[
+					"Viết ca khúc “촣아요 ‘(Like It) trong chưa đầy 40 phút.",
+					"Trong 10 năm tới muốn cả thế giới biết đến âm nhạc của mình.",
+					"Khi còn nhỏ có ước mơ trở thành lính cứu hỏa, cầu thủ bóng rổ và nhạc sĩ.",
+					"Từng phải phẫu thuật viêm ruột thừa (tháng 12/2013)",
+					"Mong muốn có một con chó loài Weish Corgi.",
+					"Quyết định sẽ thành rapper sau khi nghe bài rap “Ragga Muffin” của Stony Skunk (YG Entertainment) và “Fly” của nhóm Epik High.",
+					"Được coi như là người bố trong nhóm, chuyên sửa chữa những thiết bị hỏng trong ký túc xá.",
+					"Khi lo lắng hay gặp những việc khó khăn sẽ tâm sự cùng với Rap Monster vì hai người cùng tuổi với nhau.",
+					"Khi tức giận sẽ nghe “Hold me Back” của Rick Ross.",
+					"Thường căng thẳng từ lúc nửa đêm cho đến 6 giờ sáng (Suga thường làm việc trong thời gian này)."
+					]
+			},
+
+        ]
+    },
+    //--------------------------------------------------------------------------
+    {
+        id: 'JungKook',
+        srcImg: 'src/img/background/JungKook.jpg',
+        info: [
+            {
+                title: 'Thông tin cá nhân: JungKook',
+                content: [
+                    "Tên thật của  Jin trong nhóm BTS: Kim Seok Jin",
+                    "Nghệ danh : Pink Princess, Jin Princess, Anh Vai Đẹp, Cỗ Máy Nhảy, Eat Jin.",
+                    "Ngày sinh : 04/12/1992 —-> Nhân Mã",
+                    "Vị trí : Vocalist, Visual (gương mặt của nhóm)",
+                    "Nhóm máu : O",
+                    "Chiều cao: 1m79cm",
+                    "Cân nặng: 60kg",
+                    "Công ty chủ quản: Big Hit Entertainment, Pony Canyon."
+                ]
+            },
+
+			{
+                title: 'Giải thưởng',
+                content:[
+					"MelOn Music Award : New artist of the year(2013)",
+					"Golden Disk Award: Newcomer Award(2014)",
+					"MelOn Music Award: Best Male Dance Award(2015)",
+					"Golden Disk Award: Disk Bongsang (2016)",
+                ]
+            },
+
+            {
+                title:'Fun Facts',
+                content:[
+                    "Nấu ăn ngon nhất nhóm.",
+                    " Giỏi tiếng Quan Thoại (tiếng phổ thông Trung Quốc)",
+                    " Thích các nhân vật công chúa và chó con của Disney.",
+                    "Anh ấy là người duy nhất tham gia thử giọng khi vẫn đang đi học.",
+                    "Theo Jin, khi anh ấy còn là học sinh, anh ấy khá trầm tính nhưng cũng không (đến mức) phải ngồi ăn một mình",
+                    " Jin nói rằng sẽ hẹn hò với Jimin nếu Jin là con gái bởi vì anh ấy không có tính hướng ngoại cao, vậy nên sẽ tốt hơn nếu hẹn hò với 1 người như Jimin.",
+                    " Bây giờ Jin đang học cách làm diễn viên bằng cách học mấy kiểu quyến rũ các ahjumma (thím/ gái chưa chồng) và tất cả các eomma (bà mẹ/ bà nội trợ)",
+                    "Thánh vụng về của BTS, vụng về trong mọi chuyện =))",
+                    "Ham ăn nhất nhóm"
+                ]
+            }
+        ]
+    },
+	//-------------------------------------------------------------------
+    {
+        id:'Jimin',
+        srcImg:'src/img/background/Jmin.jpg',
+        info:[
+            {
+                title:'Thông tin cá nhân: Jimin',
+                content:[
+                    "Tên thật : Park Jimin",
+                    "Biệt danh : ChimChim, Chim Lùn, Con Heo, Mều",
+					"Ngày sinh: 13/10/1995 (21 tuổi)",
+					"Cung hoàng đạo: Thiên Bình",
+					"Tên Tiếng Trung: 朴智旻    (Park Ji Min)",
+					"Nhóm máu: A",
+					"Chiều cao: 1m73",
+					"Cân nặng: 60kg",
+					"Công ty chủ quản: Big Hit Entertainment, Pony Canyon."
+                ]
+            },
+
+			 {
+                title: 'Giải thưởng',
+                content:[
+                    "MelOn Music Award : New artist of the year(2013)",
+					"Golden Disk Award: Newcomer Award(2014)",
+					"MelOn Music Award: Best Male Dance Award(2015)",
+					"Golden Disk Award: Disk Bongsang (2016)"
+                ]
+            },
+
+			{
+                title:'Fun Facts',
+                content:[
+                    "Con số yêu thích: 3",
+					"Bộ phận tự tin nhất: đôi mắt",
+					"Giống như một người “mẹ” trong BTS vì anh rất hay nhắc nhở, chăm lo cho các thành viên khác.",
+					"Rất gần gũi với Jungkook.",
+					"Có nhiều kiến thức về sức khỏe (dưỡng da là tiêu biểu).",
+					"Nếu có người yêu, Jimin sẽ cho cô ấy tất cả mọi thứ. Và đầu tiên cậu ấy muốn nắm tay cô.",
+					"Nghiện luyện tập, thường chỉ ngủ 3 giờ mỗi ngày vì tập luyện quá nhiều.",
+					"Nếu có một sức mạnh siêu nhiên, anh sẽ muốn nói chuyện với chó và động vật.",
+					"Đối với Jimin, bút kẻ mắt là cuộc sống.Khi anh luyện tập mà không có bút kẻ mắt, anh cho rằng mình không thể biểu hiện sự mạnh mẽ và không thể nhảy những điệu nhảy của hip-hop.",
+					"Nhân vật yêu thích trong Avengers là Hulk.",
+					"Thích selfie.",
+					"Thích trời vừa có nắng và lạnh.",
+					"Rap Monster đã chọn Jimin là thành viên nam tính nhất nhóm."
+                ]
+            }
+        ]
+    },
+	//--------------------------------------------------------------------------
+	{
+        id:'RM',
+        srcImg:'src/img/background/RM.jpg',
+        info:[
+            {
+                title:'Thông tin cá nhân: RM',
+                content:[
+                    "Ngày sinh: 12/09/1994 (22 tuổi)",
+					"Cung hoàng đạo: Xử Nữ",
+					"Chiều cao: 1m81",
+					"Cân nặng: 64kg",
+					"Tên tiếng Trung: 金南俊 (Kim Nam Joon)",
+					"Công ty quản lý: Big Hit Entertainment",
+				]
+            },
+
+			 {
+                title: 'Giải thưởng',
+                content:[
+                   	"MelOn Music Award : New artist of the year(2013)",
+					"Golden Disk Award: Newcomer Award(2014)",
+					"MelOn Music Award: Best Male Dance Award(2015)",
+					"Golden Disk Award: Disk Bongsang (2016)"
+                ]
+            },
+
+			{
+                title:'Fun Facts',
+                content:[
+                   "Có một người em gái.",
+					"Là người thông minh nhất trong nhóm.",
+					"Là một rapper nổi tiếng.",
+					"Fanclub của Monster tên là A.R.M.Y",
+					"Theo đạo Thiên Chúa Giaso.",
+					"Hạnh phúc khi đọc sách.",
+					"Cảm thấy chán khi ở nhà một mình.",
+					"Anh cảm thấy giận dữ khi rap không tốt.",
+					"Phấn khích khi biểu diễn trước các fan.",
+					"Nói được tiếng Anh, Nhật tốt.",
+					"Thích trang điểm tự nhiên.",
+					"Không có hình xăm nào trên cơ thể.",
+					"Chỉ số IQ là 148.",
+					"Có số điểm TOIEC là 850.",
+					"Thuộc top 1% điểm thi đại học cao nhất.",
+					"Trông rất “ngầu” nhưng là một người hiền lành."
+                ]
+            }
+        ]
+    },
+	//-------------------------------------------------------------------------
+	{
+        id:'Jin',
+        srcImg:'src/img/background/Jin.jpg',
+        info:[
+            {
+                title:'Thông tin cá nhân: Jin',
+                content:[
+                   "Ngày sinh: 04/12/1992 (23 tuổi)",
+					"Cung hoàng đạo: Nhân Mã.",
+					"Tên Tiếng Trung: 金碩珍  (Park Ji Min)",
+					"Nhóm máu: O",
+					"Chiều cao: 1m79",
+					"Cân nặng: 60kg",
+					"Công ty chủ quản: Big Hit Entertainment, Pony Canyon."
+                ]
+            },
+
+			 {
+                title: 'Giải thưởng',
+                content:[
+                   	"MelOn Music Award : New artist of the year (2013)",
+					"Golden Disk Award: Newcomer Award (2014)",
+					"Gaon Chart K-Pop Award: ￼New Artist of the Year - Male Group(2014)",
+					"MelOn Music Award: Best Male Dance Award (2015)",
+					"Gaon Chart K-Pop Award: ￼World Rookie Award (2015)",
+					"Golden Disk Award: Disk Bongsang (2016)",
+					"Seoul Music Award: Bonsang Award (2016)",
+					"Gaon Chart K-Pop Award: ￼K-Pop World Hallyu Star Award (2016)",
+				]
+            },
+
+			{
+                title:'Fun Facts',
+                content:[
+					"Size giày: 8-8.5 (USA).",
+					"Con số yêu thích: 4.",
+					"Thời tiết yêu thích là nắng vào mùa xuân.",
+					"Thói quen: giật mắt trái khi đói.",
+					"Rất hạnh phúc khi có người khen vẻ điển trai của mình.",
+					"Điểm quyến rũ trên cơ thể là đôi môi.",
+					"Không thích xem phim kinh dị.",
+					"Thích các nhân vật công chúa và chó con trong Disney.",
+					"Có một chú chó tên là JJanggu.",
+					"Muốn có con đầu là con gái, sau đó là con trai.",
+					"Trong 10 năm tới, muốn thực hiện nghĩa vụ quân sự, đạt được cấp bậc trung sĩ và thành công cùng BTS.",
+					"J-Hope rất thích các món súp Jin làm.",
+					"Thường dậy sớm 2 tiếng để chuẩn bị bữa ăn cho các “con” của mình.",
+					"Luôn đi tập gym cùng với Jimin.",
+					"V là thành viên mà Jin thân thiết nhất trong nhóm."
+                ]
+            }
+        ]
+    },
+	//-------------------------------------------------------------------------
+	{
+        id:'J-Hope',
+        srcImg:'src/img/background/J-Hope.jpg',
+        info:[
+            {
+                title:'Thông tin cá nhân: J-Hope',
+                content:[
+                   "Ngày sinh: 18/02/1994 (22 tuổi)",
+					"Cung hoàng đạo: Bảo Bình",
+					"Tên Tiếng Trung:  鄭號錫   (Jung Ho Seok)",
+					"Nhóm máu: A",
+					"Chiều cao: 1m77",
+					"Cân nặng: 59kg",
+					"Công ty chủ quản: Big Hit Entertainment, Pony Canyon."
+                ]
+            },
+
+			 {
+                title: 'Giải thưởng',
+                content:[
+                   "MelOn Music Award : New artist of the year(2013)",
+					"Golden Disk Award: Newcomer Award(2014)",
+					"MelOn Music Award: Best Male Dance Award(2015)",
+					"Golden Disk Award: Disk Bongsang (2016)",
+                ]
+            },
+
+			{
+                title:'Fun Facts',
+                content:[
+                   "Là dancer tốt nhất trong nhóm.",
+					"Mọi người nhận xét J-Hope rất thân thiện.",
+					"Giống như một người “mẹ” trong BTS vì anh rất hay nhắc nhở, chăm lo cho các thành viên khác.",
+					"Rất gần gũi với Jungkook.",
+					"V nói J-Hope giống như Doraemon vì cậu ấy luôn tìm ra giải pháp.",
+					"Khuôn mặt dài 27cm.",
+					"Có một con chó tên là Mickey.",
+					"Ghét tập thể dục.",
+					"Yêu thích mùa xuân.",
+					"Khi căng thẳng sẽ nhìn lên bầu trời."
+                ]
+            }
+        ]
+    },
+]
+
+// console.log(info[0].info[2].title);
+
+for (var i = 0; i < info.length; ++i) {
+    var parentDIV = document.getElementById(info[i].id);
+    var myImg = document.createElement("img");
+    myImg.src = info[i].srcImg;
+    parentDIV.appendChild(myImg);
+    for (var j = 0; j < info[i].info.length; ++j) {
+        var myHeader = document.createElement("h1");
+        myHeader.innerHTML = info[i].info[j].title;
+        parentDIV.appendChild(myHeader);
+        var myUL = document.createElement("ul");
+        parentDIV.appendChild(myUL);
+        for (var k = 0; k < info[i].info[j].content.length; ++k) {
+            var myLI = document.createElement("li");
+            myLI.innerHTML = info[i].info[j].content[k];
+            myUL.appendChild(myLI);
+        }
 
 
-function showcontent(x){
-
-  if(window.XMLHttpRequest) {
-    xmlhttp = new XMLHttpRequest();
-  } else {
-    xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');
-  }
-
-  xmlhttp.onreadystatechange = function() {
-    if(xmlhttp.readyState == 1) {
-        document.getElementById('content').innerHTML = "<i class='huge notched circle loading icon' style='margin:auto auto;'></i>";
     }
-    if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-      document.getElementById('content').innerHTML = xmlhttp.responseText;
-    }
-  }
-
-  xmlhttp.open('POST', x+'.html', true);
-  xmlhttp.setRequestHeader('Content-type','application/x-www-form-urlencoded');
-  xmlhttp.send(null);
-
 }
