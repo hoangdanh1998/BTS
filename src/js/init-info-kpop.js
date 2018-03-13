a038fb7a856580b68a0d8f4bcff9d6ca63d34bcd
Hoang Danh <hoangdanhlx1998@gmail.com>
2018-03-13 11:19:43 +0700

    fix-src and add profile.html

--- src/js/init-info-kpop.js
+++ src/js/init-info-kpop.js
@@ -1,7 +1,7 @@
 var info = [
     {
-        id: 'V',
-        srcImg: 'src/img/BackGround/V.jpg',
+        id: 'v',
+        srcImg: 'src/img/background/v.jpg',
         info: [
             {
                 title: 'Thông tin cá nhân: V',
@@ -12,10 +12,7 @@ var info = [
                     "Nhóm máu: AB",
                     "Chiều cao: 1m79",
                     "Cân nặng: 58kg",
-                    "Công ty chủ quản: Big Hit Entertainment, Pony Canyon.",
-                    "Facebook: https://www.facebook.com/Taehyung.02/",
-                    "Instagram: https://www.instagram.com/kingtaehyung/",
-                    "Trang web chính thức: http://bts.ibighit.com/"
+                    "Công ty chủ quản: Big Hit Entertainment, Pony Canyon."
                 ]
             },
             // {
@@ -32,19 +29,19 @@ var info = [
             //
             //     ]
             // },
-            {
-                title: 'Giải thưởng:',
-                content:[
-                    "MelOn Music Award : New artist of the year (2013)",
-                    "Golden Disk Award: Newcomer Award (2014)",
-                    "Gaon Chart K-Pop Award: ￼New Artist of the Year (Male Group) (2014)",
-                    "MelOn Music Award: Best Male Dance Award (2015)",
-                    "Gaon Chart K-Pop Award: ￼World Rookie Award (2015)",
-                    "Golden Disk Award: Disk Bongsang (2016)",
-                    "Seoul Music Award: Bonsang Award (2016)",
-                    "Gaon Chart K-Pop Award: K-Pop World Hallyu Star Award (2016)"
-                ]
-            },
+            // {
+            //     title: 'Giải thưởng:',
+            //     content:[
+            //         "MelOn Music Award : New artist of the year (2013)",
+            //         "Golden Disk Award: Newcomer Award (2014)",
+            //         "Gaon Chart K-Pop Award: ￼New Artist of the Year (Male Group) (2014)",
+            //         "MelOn Music Award: Best Male Dance Award (2015)",
+            //         "Gaon Chart K-Pop Award: ￼World Rookie Award (2015)",
+            //         "Golden Disk Award: Disk Bongsang (2016)",
+            //         "Seoul Music Award: Bonsang Award (2016)",
+            //         "Gaon Chart K-Pop Award: K-Pop World Hallyu Star Award (2016)"
+            //     ]
+            // },
             {
                 title: 'Fun Facts:',
                 content:[
@@ -61,7 +58,7 @@ var info = [
     },
     // -----------------------------------------------------------------------------------------------
     {
-        id: 'Suga',
+        id: 'suga',
         srcImg: 'src/img/background/suga.jpg',
         info: [
             {
@@ -73,10 +70,7 @@ var info = [
                     "Nhóm máu: AB",
                     "Chiều cao: 1m79",
                     "Cân nặng: 58kg",
-                    "Công ty chủ quản: Big Hit Entertainment, Pony Canyon.",
-                    "Facebook: https://www.facebook.com/Taehyung.02/",
-                    "Instagram: https://www.instagram.com/kingtaehyung/",
-                    "Trang web chính thức: http://bts.ibighit.com/"
+                    "Công ty chủ quản: Big Hit Entertainment, Pony Canyon."
                 ]
             },
             // {
@@ -92,16 +86,20 @@ var info = [
             //     ]
             // },
             {
-                title: 'Giải thưởng',
+                title: 'Fun Facts',
                 content:[
-                    "MelOn Music Award : New artist of the year (2013)",
-                    "Golden Disk Award: Newcomer Award (2014)",
-                    "Gaon Chart K-Pop Award: ￼New Artist of the Year (Male Group) (2014)",
-                    "MelOn Music Award: Best Male Dance Award (2015) ",
-                    "Gaon Chart K-Pop Award: ￼World Rookie Award (2015)",
-                    "Golden Disk Award: Disk Bongsang (2016)",
-                    "Seoul Music Award: Bonsang Award (2016)",
-                    "Gaon Chart K-Pop Award: K-Pop World Hallyu Star Award (2016)"
+                    "Suga về nhì trong buổi thử giọng cuối cùng của BigHit năm 2010",
+                    "Theo như Jimin nói, Suga rất thích trêu đùa mọi người và là người nắm bắt thông tin khá nhạy",
+                    " Suga tự thấy mình giống Welsh Corgi (tên một giống chó)",
+                    "Thích kiểu thời tiết mà có thể mặc áo cộc tay vào ban ngày và áo dài tay vào ban đêm",
+                    "Tên của Suga được đặt bởi công ty vì họ nghĩ rằng Suga có nụ cười ngọt ngào như đường vậy",
+                    "Suga từng 1 lần biểu diễn ở quê và sau lần đó anh quyết định trở thành nghệ sĩ",
+                    "Suga viết lời bài hát “I Like It” trong single debut “2 COOL 4 SKOOL” của BTS trong vòng chưa đầy 40 phút",
+                    "Suga có phong cách swag (kiểu cool ngầu)",
+                    "Nếu xét về độ nam tính RapMon no.1 thì Suga sẽ là no.2",
+                    "Suga sẽ hẹn hò với Jin nếu Suga là con gái",
+                    "Rất thích nhiếp ảnh.",
+                    "Suga rất giỏi chơi các môn thể thao."
                 ]
             }
 
@@ -109,11 +107,11 @@ var info = [
     },
     //--------------------------------------------------------------------------
     {
-        id: 'JungKook',
-        srcImg: 'src/img/background/JungKook.jpg',
+        id: 'jin',
+        srcImg: 'src/img/background/jin.jpg',
         info: [
             {
-                title: 'Thông tin cá nhân: JungKook',
+                title: 'Thông tin cá nhân: Jin',
                 content: [
                     "Tên thật của  Jin trong nhóm BTS: Kim Seok Jin",
                     "Nghệ danh : Pink Princess, Jin Princess, Anh Vai Đẹp, Cỗ Máy Nhảy, Eat Jin.",
@@ -140,42 +138,188 @@ var info = [
                 ]
             }
         ]
-        },
+    },
     {
-        id:'Jimin',
+        id:'jimin',
         srcImg:'src/img/background/Jmin.jpg',
         info:[
             {
                 title:'Thông tin cá nhân: Jimin',
                 content:[
                     "Tên thật : Park Jimin",
-                    "Biệt danh : ChimChim, Chim Lùn, Con Heo, Mều"
+                    "Biệt danh : ChimChim, Chim Lùn, Con Heo, Mều",
+                    "Ngày sinh : 13/10/1995 —-> Thiên Bình ",
+                    "Vị trí : Lead Vocalist, Lead Dancer",
+                    "Nhóm máu : A",
+                    "Màu yêu thích : Xanh da trời nhạt (xanh lam)",
+                    "Số yêu thích : 3",
+                    "Chiều cao : 175 cm (lùn nhất nhóm)",
+                    "Cân nặng : 60 kg",
+                    "Gia đình : bố, mẹ, em trai",
+                    "Hình mẫu lý tưởng : Đẹp và đáng yêu, dáng người phải nhỏ hơn anh ấy"
+                ]
+            },
+            {
+                title:'Fun Facts',
+                content:[
+                    " Thích chụp hình tự sướng",
+                    "Thích trời vừa có nắng và lạnh.",
+                    "Thích đeo tai nghe vì nó đem lại cảm giác rất dễ chịu.",
+                    "Trong 1 vlog (video blog) của BTS, J-Hope nói rằng Jimin thường xuyên đánh các hyung nhưng luôn thoát tội vì sự đáng yêu của mình",
+                    "Thân nhất với Jungkook (fan cuồng của Kookie~).",
+                    "Là một trong những thành viên nói nhiều của nhóm.",
+                    "RapMon đã chọn Jimin là thành viên nam tính nhất nhóm.",
+                    "Jimin nghĩ rằng Jungkook khá là đáng yêu nhưng vì các mem khác cho rằng nói thế nghe thật kì cục nên Jimin không thường khen vậy nhiều.",
+                    "Tự gọi mình là heo và nghĩ rằng mình thật béo. Hơi tự ti về má của mình.",
+                    "Jimin trúng tuyển thông qua vòng thử giọng.",
+                    " Trong “American Hustle Life”, có anh nước ngoài cứ luôn miệng gọi Jimin là ChimChim >< với mật độ rất ư là thường xuyên",
+                    "Trong số các mem thì Jimin là người chỉ training có 1 năm, còn các mem khác thì khoảng 3 năm.",
+                    "Jimin nói rằng fan chính là hình mẫu lý tưởng của anh. Thế rồi Suga chen vào : “Hình mẫu lý tưởng của Jimin là Min Yoongi (tên thật của Suga)"
                 ]
             }
         ]
-    }
+    },
+    {
+        id:'rm',
+        srcImg:'src/img/background/RM.jpg',
+        info:[
+            {
+                title:'Thông tin cá nhân: RM',
+                content:[
+                "Tên thật : Kim Nam Joon",
+"Biệt danh : RapMon, Leader Mon, Ráp Mông, Dập Mông",
+"Ngày sinh : 12/09/1994 —> Xử Nữ",
+"Vị trí : Leader, Lead Rapper",
+"Nhóm máu : A",
+"Chiều cao : 181 cm (cao nhất nhóm)",
+"Cân nặng : 64 kg (nặng nhất nhóm)",
+"Gia đình : Bố, mẹ, em gái",
+"Số yêu thích : 1"
+
+                ]
+            },
+            {
+                title:'Fun Facts',
+                content:[
+                    "Tự học tiếng Anh.",
+"Thích đi uống cafe để tìm cảm hứng.",
+"Thích lướt web, thích thời tiết trong lành.",
+"Công khai ủng hộ quyền lợi của LGBT.",
+"Đã sáng tác/ viết hơn 100 bài hát.",
+"Suga và RapMon rất thích viết lyric cho các bài hát của BTS, nhưng các thành viên khác cũng sẽ giúp họ viết.",
+"RapMon từng nói rằng khi anh ấy còn ở trong Mix Tape, có fan đã hét lên 'Rap Monster' nên anh đã dùng nó làm nghệ danh.",
+"RapMon bắt đầu tập viết lyric khi anh đang đi học vào vở của mình.",
+"Fan muốn RapMon tháo kính ra, anh trả lời rằng sẽ làm vậy khi nào anh có kiểu tóc đẹp hơn.",
+"RapMon chọn J-Hope để hẹn hò nếu RapMon là con gái vì J-Hope giống như “bà mẹ” của cả nhóm vậy.",
+"Khi các thành viên BTS được hỏi 'Ai là thành viên nam tính nhất của BTS ?', đa số đã chọn RapMon."
+                ]
+            }
+        ]
+    },
+    {
+        id:'jungkook',
+        srcImg:'src/img/background/JungKook.jpg',
+        info:[
+            {
+                title:'Thông tin cá nhân: JungKook',
+                content:[
+                    "Tên thật : Jeon Jungkook",
+"Biệt danh : Golden Maknae, Cúc-ki, Cúc, Kookie",
+"Ngày sinh : 01/09/1997 —-> Xử Nữ",
+"Vị trí : Main Vocalist, Main Dancer, Rapper, Maknae.",
+"Nhóm máu : A",
+"Sở trường : Vẽ",
+"Màu yêu thích : Đỏ",
+"Số yêu thích : 1",
+"Chiều cao : 178 cm",
+"Cân nặng : 61 kg",
+"Gia đình : bố, mẹ, anh trai"
+
+                ]
+            },
+            {
+                title:'Fun Facts',
+                content:[
+                    "Năm 2012, Jungkook theo học LA Summer Of 2012 cho khóa huấn luyện tương tự Movement Lifestyle (một học viên nhảy nổi tiếng)",
+"Thích thời tiết có nắng và gió mát",
+"Jungkook sợ hãi khi các hyung bắt cậu làm các động tác dễ thương",
+"Trong ngày Valentine, Jungkook nói rằng cậu nhận được chocolate từ 2 cô gái",
+"Sở thích là Dancing và Skateboarding.",
+"Jungkook được khen về kĩ năng nhảy của cậu bởi một dancer nổi tiếng rằng cậu giỏi như Kyle Hanagami (nhảy chính đứng thứ 3 của nhóm, sau J-Hope thứ nhất và Jimin thứ hai).",
+"Jungkook với tư cách là maknae luôn chiếm thời gian lâu nhất trong phòng tắm.",
+"Jungkook khi được yêu cầu chọn ra mem thích nhất trong nhóm, cả Jimin và J-Hope đều làm kí hiệu tự chỉ vào mình, nhưng Jungkook không đưa ra câu trả lời.",
+ "Giống như Suga, Jungkook nói rằng cậu sẽ hẹn hò với Jin nếu em ấy là con gái."
+                ]
+            }
+        ]
+    },
+
+    {   id:'jhope',
+        srcImg:'src/img/background/jhope.jpg',
+    info:[
+        {
+            title:"Thông tin cá nhân: J-Hope",
+            content:[
+                "Tên thật : Jung Ho Seok",
+                "Nghệ danh : (tên đại diện cho hy vọng nhưng luôn bị troll đến tuyệt vọng xD) Hốp, Hobbie, Hopie, J-Horse",
+                "Ngày sinh : 18/02/1994 —-> Bảo Bình",
+                "Vị trí : Rapper và Lead Dancer (nhảy đẹp nhất trong BTS)",
+                "Nhóm máu : A",
+                "Màu yêu thích : Xanh lá cây",
+                "Số yêu thích : 7",
+                "Chiều cao : 177 cm",
+                "Cân nặng : 59 kg",
+                "Gia đình : Bố, mẹ, chị gái",
+                "Hình mẫu lý tưởng : 'Someone who likes me!' = Ai cũng được miễn là thích tôi!"
+            ]
+        },
+        {
+            title:'Fun Facts',
+            content:[
+                "Gia nhập nhóm BTS năm 2011",
+                " Theo Jimin, J-hope đảm nhận vai trò mẹ của cả nhóm, anh ấy cũng hay nấu ăn cho các mem.",
+                " Thích chó và cún con, thích mùa xuân.",
+                " J-Hope là khách mời và dancer phụ họa trong MV 'Im Da One' của Jokwon và cũng xuất hiện trong 'Animal' của Jokwon.",
+                " Anh là trainee chính thức của JYP.",
+                " J-Hope và Youngjae (nhóm B.A.P) đã tham gia cùng một buổi thử giọng của JYP.",
+                " J-Hope muốn Jungkook dạy cho mình cách làm những động tác dễ thương.",
+                " J-Hope trước đây là dancer underground và đó chính là động lực thúc đẩy anh trở thành 1 nghệ sĩ.",
+                " Suga nói rằng J-Hope và Jungkook rất thân thiết với nhau. Họ có mối quan hệ như là cha và con. Mỗi khi Jungkook khóc, đặc biệt khi họ còn là trainee, J-Hope sẽ đến an ủi thằng bé "
+            ]
+        }
+    ]
+
+}
 ]
 
 // console.log(info[0].info[2].title);
 
 for (var i = 0; i < info.length; ++i) {
     var parentDIV = document.getElementById(info[i].id);
+    parentDIV.className = "ui piled segment";
     var myImg = document.createElement("img");
-    myImg.src = info[i].srcImg;
     parentDIV.appendChild(myImg);
+
+    myImg.src = info[i].srcImg;
     for (var j = 0; j < info[i].info.length; ++j) {
-        var myHeader = document.createElement("h1");
+
+        var childDiv = document.createElement("div");
+        childDiv.className="ui piled segment";
+        parentDIV.appendChild(childDiv);
+
+        var myHeader = document.createElement("h3");
         myHeader.innerHTML = info[i].info[j].title;
-        parentDIV.appendChild(myHeader);
+        childDiv.appendChild(myHeader);
 
         var myUL = document.createElement("ul");
-        parentDIV.appendChild(myUL);
+        childDiv.appendChild(myUL);
         for (var k = 0; k < info[i].info[j].content.length; ++k) {
             var myLI = document.createElement("li");
             myLI.innerHTML = info[i].info[j].content[k];
             myUL.appendChild(myLI);
         }
-
-
     }
+
+
+
 }
